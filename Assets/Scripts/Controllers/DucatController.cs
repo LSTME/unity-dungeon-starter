bed2a4be1475d48de0b5f27db1bb1d5865b8317f
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2017-08-11 23:20:45 +0200

    Implemented ducat without any interaction logic

---
+++ Assets/Scripts/Controllers/DucatController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Controllers
+{
+    public class DucatController : MonoBehaviour
+    {
+        public float RotationSpeed = 350.0f;
+        public float BouncingSpeed = 150.0f;
+        public float BouncingAmplitude = 0.05f;
+
+        private float _angle = 0.0f;
+        
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, -RotationSpeed * Time.deltaTime);
+
+            _angle += BouncingSpeed * Time.deltaTime;
+            _angle = _angle > 360.0f ? _angle - 360.0f : _angle;
+            var bounceHeight = BouncingAmplitude * Mathf.Sin(_angle * Mathf.PI / 180.0f);
+            transform.position = new Vector3(transform.position.x, bounceHeight, transform.position.z);
+        }
+    }
+}
+
+

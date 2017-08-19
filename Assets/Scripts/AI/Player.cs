528c0b95b853f73fa7542a51de1a9507015c4609
Michal Puškel <michal.puskel@gmail.com>
2017-08-20 00:23:40 +0200

    cvicenie3 - Zistite koľko najviac LEDcoinov sa nachádzalo v nejakej truhlici?

--- Assets/Scripts/AI/Player.cs
+++ Assets/Scripts/AI/Player.cs
@@ -2,13 +2,43 @@
 {
     public class Player : PlayerBase
     {
+        private int maxChest = -1;
+
         public void Start()
         {
-            StartMap("TestChest");
+            StartMap("cvicenie3");            
         }
 
         public void Tick()
         {
+            if (IsKeyDown("w"))
+            {
+                MoveForward();
+            }
+            else if (IsKeyDown("s"))
+            {
+                MoveBackward();
+            }
+            else if (IsKeyDown("a"))
+            {
+                TurnLeft();
+            }
+            else if (IsKeyDown("d"))
+            {
+                TurnRight();
+            }
+            else if (IsKeyDown("space"))
+            {
+                var coinsBefore = GetNumberOfCollectedCoins();
+                UseBlock();
+                var coinsAfter = GetNumberOfCollectedCoins();
+                var deltaCoins = coinsAfter - coinsBefore;
+                if (deltaCoins > maxChest)
+                {
+                    maxChest = deltaCoins;
+                }
+                ShowMessage(string.Format("Vybral si práve {0}\nmax truhlica bola zatiaľ {1}", deltaCoins, maxChest));                
+            }                    
         }
     }
 }

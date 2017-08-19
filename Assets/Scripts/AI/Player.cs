6ae9f74ff5ce71261027576cdafbef81f855c191
Andrej Jursa <dragoo@thelambda.sk>
2017-08-20 00:40:36 +0200

    Clean up.

--- Assets/Scripts/AI/Player.cs
+++ Assets/Scripts/AI/Player.cs
@@ -2,8 +2,6 @@
 {
     public class Player : PlayerBase
     {
-        private int maxChest = -1;
-
         public void Start()
         {
             StartMap("cvicenie3");            
@@ -11,34 +9,6 @@
 
         public void Tick()
         {
-            if (IsKeyDown("w"))
-            {
-                MoveForward();
-            }
-            else if (IsKeyDown("s"))
-            {
-                MoveBackward();
-            }
-            else if (IsKeyDown("a"))
-            {
-                TurnLeft();
-            }
-            else if (IsKeyDown("d"))
-            {
-                TurnRight();
-            }
-            else if (IsKeyDown("space"))
-            {
-                var coinsBefore = GetNumberOfCollectedCoins();
-                UseBlock();
-                var coinsAfter = GetNumberOfCollectedCoins();
-                var deltaCoins = coinsAfter - coinsBefore;
-                if (deltaCoins > maxChest)
-                {
-                    maxChest = deltaCoins;
-                }
-                ShowMessage(string.Format("Vybral si práve {0}\nmax truhlica bola zatiaľ {1}", deltaCoins, maxChest));                
-            }                    
         }
     }
 }
